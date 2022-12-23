4b2ae9fc75366dc79059506087ed36c90293ff9b
diegouidev <diegocoelho44@gmail.com>
2022-12-22 21:28:21 -0300

    add input/button

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,28 @@
-import { View, Text, StatusBar } from 'react-native'
-import { styles } from './styles' 
+import { View, TextInput, TouchableOpacity} from 'react-native'
+import { styles } from './styles'
+import Logo from '../../assets/Logo.svg'
+import Add from '../../assets/add.svg'
 
 
 export function Home() {
   return (
-    <View style={styles.container}>
-      
-      <Text style={styles.header}>Olá Mundo!</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+          <View style={styles.header}>
+            <Logo style={styles.logo}/>
+          </View>
+          <View style={styles.form}>
+            <TextInput
+              style={styles.input}
+              placeholder="Adicione uma nova tarefa"
+              placeholderTextColor="#6b6b6b"
+            />
+
+            <TouchableOpacity style={styles.button}>
+              <Add width={20} height={20} />
+            </TouchableOpacity>
+          </View>
+      </View> 
+    </>
   )
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -8,6 +8,35 @@ export const styles = StyleSheet.create({
   header: {
     height: 173,
     backgroundColor: '#0D0D0D',
-    fontSize: 36,
-  }
+  },
+  logo: {
+    marginTop: 72,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: -54,
+    marginBottom: 42,
+    padding: 24
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: '#1f1e25',
+    borderRadius: 5,
+    color: '#fff',
+    padding: 16,
+    marginRight: 7
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#1E6F9F',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 });
