8d6bc94ab49d13722caa319404cc5c6db1f83c35
diegouidev <diegocoelho44@gmail.com>
2022-12-22 23:07:58 -0300

    update

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,39 +1,22 @@
-import { useState } from 'react'
 import { View, Text, TouchableOpacity } from "react-native"
 import Trash from '../assets/trash.svg'
 import { styles } from "./style"
-import { AntDesign } from '@expo/vector-icons'
-import { Entypo } from '@expo/vector-icons'
 
-interface Tafs {
-  description: string,
-  onRemove: () => void,
-  count: (arg1: string) => void,
+type Props = {
+  name: string
+  onRemove: () => void
 }
 
-export function Task({ description, onRemove, count }: Tafs){
-  const [icontrueOrFalse, setIcontrueOrFalse] = useState(false);
-
-    function handleChecked() {
-        setIcontrueOrFalse(!icontrueOrFalse);
-        count(description);
-    }
+export function Task({ name, onRemove }: Props){
   return(
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.cardButton}
-        onPress={handleChecked}
-      >
-      {icontrueOrFalse === true ?
-        (<AntDesign name="checkcircle" size={24} color="#4EA8DE" />)
-          :
-        (<Entypo name="circle" size={24} color="#4EA8DE" />)}
-      </TouchableOpacity>
       <Text style={styles.name}>
-        {/* {name} */}
+        {name}
       </Text>
       <TouchableOpacity style={styles.button} > 
           <Trash
+            width={40}
+            height={40}
             onPress={onRemove}
           />
         </TouchableOpacity>
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,35 @@
-import { View, TextInput, TouchableOpacity, Text } from 'react-native'
+import { useState } from 'react'
+import { View, TextInput, TouchableOpacity, Text, Alert, FlatList } from 'react-native'
 import { styles } from './styles'
 import Logo from '../../assets/Logo.svg'
 import Add from '../../assets/add.svg'
-import { Task } from '../../components/'
+import { Task } from '../../components'
 
 
 export function Home() {
+  const [tasks, setTasks] = useState<string[]>([])
+  const [taskName, setTaskName] = useState('')
+
+  function handleTaskAdd(){
+    if(tasks.includes(taskName)){
+      return Alert.alert('Tarefa existe', 'Já existe uma tarefa na lista.')
+    }
+    setTasks(prevState => [...prevState, taskName])
+    setTaskName('')
+  }
+
+  function handleTaskRemove(name: string){
+    Alert.alert('Remover', `Remover a tarefa?`, [
+      {
+        text: 'Sim',
+        onPress: () => setTasks(prevState => prevState.filter(task => task !== name))
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
+  }
   return (
     <>
       <View style={styles.container}>
@@ -18,7 +42,8 @@ export function Home() {
               placeholder="Adicione uma nova tarefa"
               placeholderTextColor="#6b6b6b"
             />
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button}
+              onPress={handleTaskAdd}>
               <Add width={20} height={20} />
             </TouchableOpacity>
           </View>
@@ -40,7 +65,28 @@ export function Home() {
               </Text>
             </View>
           </View>
-          <Task />
+          <FlatList
+            data={tasks}
+            keyExtractor={item => item}
+            renderItem={({ item }) => (
+              <Task
+                key={item}
+                name={item}
+                onRemove={() => handleTaskRemove(item)}
+              />
+            )}
+            showsVerticalScrollIndicator={false}
+            ListEmptyComponent={() => (
+              <View style={{ alignItems: 'center', marginTop: 64 }}>
+                <Text
+                  style={{ fontWeight: 'bold', color: '#808080' }}>
+                  Você ainda não tem tarefas cadastradas</Text>
+                  <Text style={{ color: '#808080' }}>
+                    Crie tarefas e organize seus itens a fazer
+                  </Text>
+              </View>
+          )}
+          />
       </View> 
     </>
   )
